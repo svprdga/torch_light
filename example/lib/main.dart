e291c2172bfa26e53ca0167c3afa6335acd46d12
svprdga <svprdga@reprom.io>
2021-11-30 11:27:34 +0100

    Arrange code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,12 +14,14 @@ class TorchApp extends StatefulWidget {
 class _TorchAppState extends State<TorchApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(supportedLocales: [
-      Locale('en', '')
-    ], localizationsDelegates: [
-      GlobalMaterialLocalizations.delegate,
-      GlobalWidgetsLocalizations.delegate,
-    ], home: TorchController());
+    return MaterialApp(
+      supportedLocales: const [Locale('en', '')],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      home: TorchController(),
+    );
   }
 }
 
@@ -27,46 +29,49 @@ class TorchController extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('torch_light example app'),
-        ),
-        body: FutureBuilder<bool>(
-          future: _isTorchAvailable(context),
-          builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-            if (snapshot.hasData && snapshot.data!) {
-              return Column(
-                children: [
-                  Expanded(
-                      child: Center(
+      appBar: AppBar(
+        title: const Text('torch_light example app'),
+      ),
+      body: FutureBuilder<bool>(
+        future: _isTorchAvailable(context),
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          if (snapshot.hasData && snapshot.data!) {
+            return Column(
+              children: [
+                Expanded(
+                  child: Center(
                     child: ElevatedButton(
-                      child: Text('Enable torch'),
+                      child: const Text('Enable torch'),
                       onPressed: () async {
                         _enableTorch(context);
                       },
                     ),
-                  )),
-                  Expanded(
-                      child: Center(
+                  ),
+                ),
+                Expanded(
+                  child: Center(
                     child: ElevatedButton(
-                      child: Text('Disable torch'),
+                      child: const Text('Disable torch'),
                       onPressed: () {
                         _disableTorch(context);
                       },
                     ),
-                  )),
-                ],
-              );
-            } else if (snapshot.hasData) {
-              return Center(
-                child: Text('No torch available.'),
-              );
-            } else {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
-        ));
+                  ),
+                ),
+              ],
+            );
+          } else if (snapshot.hasData) {
+            return const Center(
+              child: Text('No torch available.'),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
   }
 
   Future<bool> _isTorchAvailable(BuildContext context) async {
@@ -74,7 +79,9 @@ class TorchController extends StatelessWidget {
       return await TorchLight.isTorchAvailable();
     } on Exception catch (_) {
       _showMessage(
-          'Could not check if the device has an available torch', context);
+        'Could not check if the device has an available torch',
+        context,
+      );
       rethrow;
     }
   }
