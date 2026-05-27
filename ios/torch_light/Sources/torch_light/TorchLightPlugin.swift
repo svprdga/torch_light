89cad6b9af6966d978ae30cb135d21bf08c01a4d
svprdga <david@davidserrano.io>
2026-05-27 17:41:26 +0200

    Enable Swift Package Manager

--- ios/torch_light/Sources/torch_light/TorchLightPlugin.swift
+++ ios/torch_light/Sources/torch_light/TorchLightPlugin.swift
@@ -2,33 +2,33 @@ import Flutter
 import UIKit
 import AVFoundation
 
-public class SwiftTorchLightPlugin: NSObject, FlutterPlugin {
-    
+public class TorchLightPlugin: NSObject, FlutterPlugin {
+
     // Constants
-    
+
     let nativeEventIsTorchAvailable = "torch_available"
     let errorIsTorchAvailable = "torch_available_error"
-    
+
     let nativeEventEnableTorch = "enable_torch"
     let errorEnableTorchExistentUser = "enable_torch_error_existent_user"
     let errorEnableTorch = "enable_torch_error"
     let errorEnableTorchNotAvailable = "enable_torch_not_available"
-    
+
     let nativeEventDisableTorch = "disable_torch"
     let errorDisableTorchExistentUser = "disable_torch_error_existent_user"
     let errorDisableTorch = "disable_torch_error"
     let errorDisableTorchNotAvailable = "disable_torch_not_available"
-    
+
     // Public methods
-    
+
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "com.svprdga.torchlight/main", binaryMessenger: registrar.messenger())
-        let instance = SwiftTorchLightPlugin()
+        let instance = TorchLightPlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
-    
+
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        switch(call.method) {
+        switch call.method {
         case self.nativeEventIsTorchAvailable:
             isTorchAvailable(result: result)
             return
@@ -42,24 +42,24 @@ public class SwiftTorchLightPlugin: NSObject, FlutterPlugin {
             result(FlutterMethodNotImplemented)
         }
     }
-    
+
     // Private methods
-    
+
     private func isTorchAvailable(result: FlutterResult) {
         guard let device = AVCaptureDevice.default(for: .video) else {
             result(FlutterError(code: self.errorIsTorchAvailable, message: "Could not determine if the device has a torch, please make sure that you are doing this on a real device.", details: nil))
             return
         }
-        
+
         result(device.hasTorch)
     }
-    
+
     private func enableTorch(result: FlutterResult) {
         guard let device = AVCaptureDevice.default(for: .video) else {
             result(FlutterError(code: self.errorEnableTorch, message: "Could not enable torch, please make sure that you are doing this on a real device", details: nil))
             return
         }
-        
+
         if device.hasTorch {
             do {
                 try device.lockForConfiguration()
@@ -68,19 +68,19 @@ public class SwiftTorchLightPlugin: NSObject, FlutterPlugin {
             } catch {
                 result(FlutterError(code: self.errorEnableTorchExistentUser, message: "Could not enable torch: ", details: nil))
             }
-            
+
             result(nil)
         } else {
             result(FlutterError(code: self.errorEnableTorchNotAvailable, message: "Torch is not available", details: nil))
         }
     }
-    
+
     private func disableTorch(result: FlutterResult) {
         guard let device = AVCaptureDevice.default(for: .video) else {
             result(FlutterError(code: self.errorDisableTorch, message: "Could not disable torch, please make sure that you are doing this on a real device", details: nil))
             return
         }
-        
+
         if device.hasTorch {
             do {
                 try device.lockForConfiguration()
@@ -89,7 +89,7 @@ public class SwiftTorchLightPlugin: NSObject, FlutterPlugin {
             } catch {
                 result(FlutterError(code: self.errorDisableTorchExistentUser, message: "Could not disable torch", details: nil))
             }
-            
+
             result(nil)
         } else {
             result(FlutterError(code: self.errorDisableTorchNotAvailable, message: "Torch is not available", details: nil))
